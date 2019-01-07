9a219beda1fa8a9b25135d934857bb891908bff3
free-bit <erkin.turkoz@metu.edu.tr>
2019-01-07 21:10:52 +0300

    key press for height factor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include "helper.h"
+#include <glm.hpp> // GL Math library header
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp> 
+#include <iostream>
+
+using namespace glm;
 
 static GLFWwindow * win = NULL;
 
@@ -11,9 +17,62 @@ GLuint idMVPMatrix;
 
 int widthTexture, heightTexture;
 
+// Uniform variable
+float heightFactor=10.0;
+
 static void errorCallback(int error,
   const char * description) {
   fprintf(stderr, "Error: %s\n", description);
+}
+
+void incrHeightFactor(){
+  heightFactor+=0.5;
+  glUniform1f(glGetUniformLocation(idProgramShader, "heightFactor"), heightFactor);
+}
+void decrHeightFactor(){
+  heightFactor-=0.5;
+  glUniform1f(glGetUniformLocation(idProgramShader, "heightFactor"), heightFactor);
+}
+
+//Added
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if(action == GLFW_PRESS){
+    switch(key) 
+    {
+      /*W, S for pitch change*/
+      case GLFW_KEY_W:
+        break;
+      case GLFW_KEY_S:
+        break;
+      /*A, D for yaw change*/
+      case GLFW_KEY_A:
+        break;
+      case GLFW_KEY_D:
+        break;
+      /*O, L for height change*/
+      case GLFW_KEY_O:
+        incrHeightFactor();
+        break;
+      case GLFW_KEY_L:
+        decrHeightFactor();
+        break;
+      /*U, J for speed*/
+      case GLFW_KEY_U:
+        break;
+      case GLFW_KEY_J:
+        break;
+      /*F for fullscreen*/
+      case GLFW_KEY_F:
+        break;
+      case GLFW_KEY_ESCAPE:
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        break;
+      /*Otherwise*/
+      default:
+        break;
+      }
+    }
 }
 
 int main(int argc, char * argv[]) {
@@ -41,6 +100,9 @@ int main(int argc, char * argv[]) {
   }
   glfwMakeContextCurrent(win);
 
+  //Added
+  glfwSetKeyCallback(win, keyCallback);
+
   GLenum err = glewInit();
   if (err != GLEW_OK) {
     fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
@@ -51,6 +113,10 @@ int main(int argc, char * argv[]) {
 
   initShaders();
   glUseProgram(idProgramShader);
+  //Added
+  //glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+  //glUniform1i(glGetUniformLocation(idProgramShader, "texturemap"), 0);
+
   initTexture(argv[1], & widthTexture, & heightTexture);
 
   while (!glfwWindowShouldClose(win)) {
